dac0bc4bb55e794bbbd7603d4f12d6291b6b56e8
Michael Marcinek <michaelmarcinek@gmail.com>
2015-05-14 18:58:12 -0400

    Saves progress before break.

--- main.js
+++ main.js
@@ -2,25 +2,35 @@
 // Define a function max() that takes two numbers as arguments and returns the largest of them. Use the if-then-else construct available in JavaScript.
 // ---------------------
 
-function max(){
-    //...
-}
+function max(a,b){
+    if(a > b) {
+        return a;
+    } else {
+        return b;
+    };
+};
 
 // ---------------------
 // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
 // ---------------------
 
-function maxOfThree(){
-    //...
-}
+function maxOfThree(a,b,c){
+    if(a > b && a > c) {
+        return a;} else if(b > a && b > c) {
+            return b;} else {
+                return c;
+            };
+        };
 
 // ---------------------
 // Write a function that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
 // ---------------------
+var vowel = ["a","e","i","o","u"];
 
 function isVowel(char){
-    //...
-}
+    if(char == [vowel]) {return true;}
+    else {return false;}
+};
 
 // ---------------------
 // Write a function translate() that will translate a text into "rÃ¶varsprÃ¥ket". That is, double every consonant and place an occurrence of "o" in between. For example, translate("this is fun") should return the string "tothohisos isos fofunon".
